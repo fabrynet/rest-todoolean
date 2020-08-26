39ac56c4597e52f76ab86465e614b5ce634a9090
Fabrizio <fabrynet.bianco@gmail.com>
2020-08-26 16:33:59 +0200

    Get dei tasks.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-// Dare all'utente, attraverso un input la possibilità di inserire una data con questo formato DD/MM/YYYY.
-// Rispondere all'utente con il giorno della settimana della data specificata.
+//GOAL: replicare quanto visto a lezione sulla todo-list permettendo all'utente di leggere tutti i task inseriti, inserirne di nuovi, eliminare quelli vecchi
 
 function addListeners() {
   $('#btn').click(insertTask);
@@ -19,12 +18,21 @@ function getTasks () {
     url: 'http://157.230.17.132:3001/todos',
     method: 'GET',
     success: function (data) {
-      console.log('data', data);
+      printTasks(data);
     },
     error: function (err) {
       console.log('err', err);
     }
   });
+}
+
+function printTasks (tasks) {
+  var target = $('#list');
+  for (var i = 0; i < tasks.length; i++) {
+    var task = tasks[i].text;
+    var id = tasks[i].id;
+    target.append(`<li><i class="fas fa-times-circle" data-id="${id}"></i>${task}</li>`);
+  }
 }
 
 function init() {
